0240d6470f66e176741c06091a451462e063c83d
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-22 21:26:10 -0300

    separando rotas

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+let express = require("express");
+let routes = express.Router();
+
+routes.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    users: [
+      {
+        name: "Henrique Teixeira",
+        email: "contato@bellmontsistema.com.br",
+        id: 1,
+      },
+    ],
+  });
+});
+
+routes.get("/admin", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    users: [],
+  });
+});
+
+module.exports = routes;
